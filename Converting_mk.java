8db5a3390612fa934060f4231f70d06535eef8d2
Anup Patil <84274237+Anup7030@users.noreply.github.com>
2024-02-23 21:33:09 +0530

    Add files via upload

---
+++ Converting_mk.java
@@ -0,0 +1,28 @@
+package Java_Practice;
+
+import java.util.*;
+
+public class Converting_mk {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		double Kilometer,meter;
+		System.out.println("Enter the Value for converting Meter to kilometer");
+		
+		 meter=in.nextDouble();
+		
+	     Kilometer=meter/1000;
+		
+		System.out.println("The converted meter to kilometer is:- "+Kilometer);
+		
+		
+		
+	}
+
+}
+
+
+
+
