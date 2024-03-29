70cb007a583a75c7600a6267a0fbaa29de48c641
Anup Patil <84274237+Anup7030@users.noreply.github.com>
2024-03-29 19:45:02 +0530

    Add files via upload

---
+++ Loop_Print.java
@@ -0,0 +1,13 @@
+package Java_Practice;
+
+public class Loop_Print {
+
+	public static void main(String[] args) {
+
+      for(int i=0;i<5;i++) {
+    	  System.out.println("Java is Sun");
+      }
+
+	}
+
+}
---
+++ Loop_printnumber.java
@@ -0,0 +1,13 @@
+package Java_Practice;
+
+public class Loop_printnumber {
+
+	public static void main(String[] args) {
+
+          for(int i=1;i<=5;i++) {
+        	  System.out.println(i);
+          }
+
+	}
+
+}
